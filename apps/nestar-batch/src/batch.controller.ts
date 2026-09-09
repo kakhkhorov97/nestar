7520d465dccec2f55163e478d53bcddb361ddb0e
Shawn <kakhkhorov97@gmail.com>
2026-09-09 22:37:56 +0900

    fix: modify job scheduler business logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,55 +1,61 @@
 import { Controller, Get, Logger } from '@nestjs/common';
-import { BatchService } from './batch.service';
 import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
+import { BatchService } from './batch.service';
 
 @Controller()
 export class BatchController {
 	private logger: Logger = new Logger('BatchController');
-
 	constructor(private readonly batchService: BatchService) {}
 
 	@Timeout(1000)
 	handleTimeout() {
-		this.logger.debug('BATCH SERVER READY!');
+		this.logger.debug('Batch server ready');
 	}
-	@Cron('00 * * * * *', { name: BATCH_ROLLBACK })
-	public async batchRollback() {
+
+	// @Interval(1000)
+	// handleInterval() {
+	// 	this.logger.debug('INTERVAL TEST');
+	// }
+
+	// @Cron('00 * * * * *', { name: 'CRON_TEST' })
+	// cronTest() {
+	// 	this.logger['context'] = 'CRON_TEST';
+	// 	this.logger.debug('EXECUTED');
+	// }
+
+	@Cron('0 0 1 * * *', { name: BATCH_ROLLBACK })
+	async batchRollback() {
 		try {
 			this.logger['context'] = BATCH_ROLLBACK;
-			this.logger.debug('EXECUTED!');
+			this.logger.debug('Executed');
 			await this.batchService.batchRollback();
 		} catch (err) {
 			this.logger.error(err);
 		}
 	}
 
-	@Cron('20 * * * * *', { name: BATCH_TOP_PROPERTIES })
-	public async batchProperties() {
+	@Cron('20 0 1 * * *', { name: BATCH_TOP_PROPERTIES })
+	async batchProperties() {
 		try {
 			this.logger['context'] = BATCH_TOP_PROPERTIES;
-			this.logger.debug('EXECUTED!');
-			await this.batchService.batchProperties();
+			this.logger.debug('Executed');
+			await this.batchService.batchTopProperties();
 		} catch (err) {
 			this.logger.error(err);
 		}
 	}
 
-	@Cron('40 * * * * *', { name: BATCH_TOP_AGENTS })
-	public async batchAgents() {
+	@Cron('40 0 1 * * *', { name: BATCH_TOP_AGENTS })
+	async batchAgents() {
 		try {
 			this.logger['context'] = BATCH_TOP_AGENTS;
-			this.logger.debug('EXECUTED!');
-			await this.batchService.batchAgents();
+			this.logger.debug('Executed');
+			await this.batchService.batchTopAgents();
 		} catch (err) {
 			this.logger.error(err);
 		}
 	}
-	/*
-  @Interval(1000)
-  handleInterval() {
-    this.logger.debug('INTERVAL TEST');
-  }
-  */
 
 	@Get()
 	getHello(): string {
